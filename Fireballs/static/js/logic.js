d3e8fed14f28363acd31ae3e09b6a23f7080551c
Max Beehler <maxbeehler@guest-10-166-93-37.wireless.umn.edu>
2019-04-02 20:17:21 -0500

    description

--- Fireballs/static/js/logic.js
+++ Fireballs/static/js/logic.js
@@ -1,21 +1,23 @@
-// Creating map object
-var myMap = L.map("map", {
-  center: [40, -95],
-  zoom: 4
-});
+// // Creating map object
+// var myMap = L.map("map", {
+//   center: [40, -95],
+//   zoom: 4
+// });
 
 // Adding tile layer to the map
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// }).addTo(myMap);
 
 // Store API query variable
 //var url = "https://ssd-api.jpl.nasa.gov/fireball.api";
 var url = "http://localhost:5000/fireballapi";
 //{{ url_for('get_all_fireballs') }}
+
+//fetch("/fireballapi").then(response=>response.json());
 
 // Grab the data with d3
 d3.json(url, function(err, response) {
@@ -23,13 +25,20 @@ d3.json(url, function(err, response) {
   if(err) console.log("error fetching data");
   console.log(response);
 
-  // Create a new marker cluster group
   var markers = L.markerClusterGroup();
+
+  var cityMarkers = [];
+  var stateMarkers = [];
+
+  // Create a new marker cluster group
+  // var markers = L.markerClusterGroup();
 
   //Loop through data
   for (var i = 0; i < response.result.length; i++) {
 
     //console.log(response)
+
+    var alt = response.result[i]['alt'];
 
     var location_x = response.result[i]['lat'];
     //console.log(location_x);
@@ -44,6 +53,7 @@ d3.json(url, function(err, response) {
     var velocity = response.result[i]['vel'];
     
     var location = [location_x, location_y];
+    
 
     if (response.result[i]['lat-dir'] == "S"){
       location_x = location_x * -1;
@@ -61,57 +71,123 @@ d3.json(url, function(err, response) {
       location_y = location_y * 1;
     }
 
-    //console.log(location_y);
-    //console.log(location);
-
-    var weather = "http://history.openweathermap.org/data/2.5/history/city?lat={" +
-     location_x + "}&lon={" + location_y + "}&appid=" + "26f870388fc77a582a83d3cf98ae92f2";
-
-    console.log(weather);
-
-    //var magnitude = response.features[i].properties.mag;
-
-    //var place = response.features[i].properties.place;
-
     // Conditionals for countries points
     var color = "";
+    var radius = "";
+
     if (energy < 50) {
       color = "green";
+      radius = 100
     }
     else if (energy < 500) {
       color = "yellow";
+      radius = 200
     }
     else if (energy < 5000) {
       color = "orange";
+      radius = 300
     }
     else {
       color = "red";
+      radius = 700
     }
-  
-    // Add circles to map
+    
+    //scaled code for radius change
+    // var radius = "";
+    // if (energy < 50) {
+    //   radius = 1
+    // }
+    markers.addLayer(L.marker([location_x, location_y])
+        .bindPopup("<h1> Energy: " + energy + "</h1> <hr> <h2> Date: " + date + "</h2> <h3> Impact: " + impact + "</h3> <h3> velocity: " + velocity + "</h3>"));
+
+        
+  // if (alt == 'null') {
+  //   alt = 1;
+  // }
+  // else {
+  //   alt = alt;
+  // }
+
+  stateMarkers.push(
     L.circle([location_x, location_y], {
-      fillOpacity: 0.75,
+      fillOpacity: 0.56,
       color: color,
-      weight: 5,
+      weight: 1,
       //scale:
       fillColor: color,
       // Adjust radius
-      
       //if (energy > 1000)
-      radius: energy * 25
-    }).bindPopup("<h1> Energy: " + energy + "</h1> <hr> <h2> Date: " + date + "</h2> <h3> Impact: " + impact + "</h3> <h3> velocity: " + velocity + "</h3>").addTo(myMap);
-  }
-    //console.log(magnitude)
+      radius: radius * 1000
+    })
+  
+);
 
-    // Check for location property
-    if (location) {
+  cityMarkers.push(
+    L.circle([location_x, location_y], {
+      fillOpacity: 0.75,
+      color: 'white',
+      weight: 0,
+      //scale:
+      fillColor: 'white',
+      // Adjust radius
+      //if (energy > 1000)
+      radius: alt * 2500
+    })
+  
+  );
+}
+  //   //console.log(magnitude)
 
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location_x, location_y])
-        .bindPopup(energy));
-    }
+  //   // Check for location property
+  //   if (location) {
 
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
+  //     // Add a new marker to the cluster group and bind a pop-up
+  //     markers.addLayer(L.marker([location_x, location_y])
+  //       .bindPopup(energy));
+  //   }
 
+  // // Add our marker cluster layer to the map
+  // myMap.addLayer(markers);
+
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+  
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+var states = L.layerGroup(stateMarkers);
+var cities = L.layerGroup(cityMarkers);
+
+
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+
+// Create an overlay object
+var overlayMaps = {
+  "Energy": states,
+  "Altitude": cities
+};
+
+// Define a map object
+var myMap = L.map("map", {
+  center: [0, 0],
+  zoom: 2,
+  layers: [streetmap, states, cities]
+});
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
+
+myMap.addLayer(markers);
 });
